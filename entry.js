8b131fc62ea28468b4e64f3c3f6adc1ed133c318
philsong <songbohr@163.com>
2012-11-23 09:27:46 +0800

    init o2o

---
+++ entry.js
@@ -0,0 +1,17 @@
+var weibo = require('weibo');
+
+// change appkey to yours
+var appkey = '2815523970';
+var secret = '3569f362e65a2d44b23e873ec0d608f2';
+var oauth_callback_url = 'your callback url';
+weibo.init('weibo', appkey, secret, oauth_callback_url);
+
+var user = { blogtype: 'weibo' };
+var cursor = {count: 20};
+weibo.public_timeline(user, cursor, function (err, statuses) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(statuses);
+  }
+});
